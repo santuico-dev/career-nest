16efe4c9e737dc0c80c0da0d35bb705a6a02dced
Josh <116621485+santuico-dev@users.noreply.github.com>
2025-06-14 23:56:21 +0800

    [JOSH_JUNE-14] FEAT: Added Docker for containerazation

--- career-nest-front/src/utils/router/router.js
+++ career-nest-front/src/utils/router/router.js
@@ -1,41 +1,34 @@
-import { createBrowserRouter } from 'react-router-dom';
-import AuthLayout from '../../Layouts/Auth/auth-layout';
-import Login from '../../Pages/Auth/login-page';
-import Register from '../../Pages/Auth/register-page';
+import { createBrowserRouter } from "react-router-dom";
+import AuthLayout from "../../Layouts/Auth/auth-layout";
+import Login from "../../Pages/Auth/login-page";
+import Register from "../../Pages/Auth/register-page";
 
 const router = createBrowserRouter([
-
     //AUTH
     {
-        path: '/',
+        path: "/",
         element: <AuthLayout />,
         children: [
             {
-                path: '/',
-                element: <Login />
+                path: "/",
+                element: <Login />,
             },
 
             {
-                path: '/register',
-                element: <Register />
-            }
+                path: "/register",
+                element: <Register />,
+            },
         ],
     },
 
     //JOB HUNTER - A USER WHO USES THE SYSTEM TO FIND A JOB
-    {
-
-    },
+    {},
 
     //RECRUITER - A USER WHO USES THE SYSTEM TO POST A JOB
-    {
-
-    },
+    {},
 
     //ADMIN - THE MAIN MODERATOR OF THE WEBSTIE
-    {
-
-    }
+    {},
 ]);
 
 export default router;
